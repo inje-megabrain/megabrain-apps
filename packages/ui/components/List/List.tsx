66657656a5228d8a83a088c7fcc1bf46700d6b45
byungjin <byungjin.dev@gmail.com>
2023-05-19 21:09:40 +0900

    feat: @megabrain/ui Add List Component

--- packages/ui/components/List/List.tsx
+++ packages/ui/components/List/List.tsx
@@ -1,5 +1,25 @@
-export const List = () => {
-  return <div>List</div>;
+interface ListProps<T> {
+  items: T[];
+  getKey(item: T, index: number): string | number;
+  children: React.FC<{ item: T; seqNo: number }>;
+}
+
+export const List = <T,>({ items, children, getKey }: ListProps<T>) => {
+  const Child = children;
+
+  return (
+    <>
+      {Array.isArray(items)
+        ? items.map((item, idx) => (
+            <Child
+              key={getKey ? getKey(item, idx) : idx}
+              item={item}
+              seqNo={idx}
+            />
+          ))
+        : null}
+    </>
+  );
 };
 
 export default List;
