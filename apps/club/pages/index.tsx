81253396a51f8b2c40930aef729be2d1b97c1cff
Younggun Park <ygpark0922@gmail.com>
2023-05-31 17:01:56 +0900

    feat: add icons on club introduction page

--- apps/club/pages/index.tsx
+++ apps/club/pages/index.tsx
@@ -2,7 +2,8 @@ import { Container, Header, Text } from '@megabrain/ui';
 import { useRouter } from 'next/router';
 import Footer from '@megabrain/ui/components/Footer/Footer';
 import globalMenu from '../constants/globalMenu';
-import Seo from '~/components/Seo'; // UI Libs
+import Seo from '~/components/Seo';
+import { FcCollaboration, FcConferenceCall, FcSearch, FcSerialTasks } from 'react-icons/fc'; // UI Libs
 //import { Clock } from '@megabrain/ui/icons'; // Icon
 
 export default function Home() {
@@ -64,11 +65,15 @@ export default function Home() {
               fontSize: '1.5rem',
               maxWidth: '800px',
               padding: '0 1rem',
+              wordBreak: 'keep-all',
+              '@mobile': {
+                textAlign: 'left',
+              },
             }}
           >
-            인제대학교 컴퓨터공학과의 학술동아리 메가브레인에 오신 것을 환영합니다! 우리는 웹 개발과
+            인제대학교 컴퓨터공학과 학술동아리 메가브레인에 오신 것을 환영합니다! 우리는 웹 개발과
             관련된 모든 것을 연구, 개발 및 프로젝트 협력에 전념하는 역동적인 커뮤니티입니다.
-            프런트엔드 설계에서 백엔드 구현 및 인프라 관리(CI, CD, K8s)에 이르기까지 디지털 세계의
+            프론트엔드 설계에서 백엔드 구현 및 인프라 관리(CI, CD, K8s)에 이르기까지 디지털 세계의
             무한한 가능성을 탐구하기 위해 존재합니다.
           </Text>
         </Container>
@@ -80,6 +85,8 @@ export default function Home() {
             justifyContent: 'center',
             alignItems: 'center',
             borderTop: '1px solid #eee',
+            flexDirection: 'column',
+            padding: '3rem 1rem',
           }}
         >
           <Text
@@ -89,14 +96,92 @@ export default function Home() {
             css={{
               fontSize: '1.5rem',
               maxWidth: '800px',
-              padding: '0 1rem',
+              padding: '1rem 1rem',
+              wordBreak: 'keep-all',
+              '@mobile': {
+                textAlign: 'left',
+              },
             }}
           >
             메가브레인은 지속적인 학습과 협업을 중요하게 생각합니다. 우리의 창의성, 기술력, 팀워크의
-            힘을 결합하여 웹 개발에서 한계를 뛰어 넘습니다. 세미나, 코딩 챌린지, 공동 프로젝트를
-            통해 빠르게 변화하는 이 분야에서 앞서 나가는 데 필요한 기술과 지식을 회원들에게
-            제공합니다.
+            힘을 결합하여 웹 개발에서 한계를 뛰어 넘습니다. 빠르게 변화하는 이 분야에서 앞서 나가는
+            데 필요한 기술과 지식을 회원들에게 제공합니다.
           </Text>
+          <Container
+            display={'flex'}
+            full={'horizontal'}
+            css={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              '@mobile': {
+                flexDirection: 'column',
+              },
+            }}
+          >
+            <Container
+              display={'flex'}
+              css={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                margin: '0 1rem',
+                '@mobile': {
+                  width: '100%',
+                  margin: '1rem 0',
+                },
+              }}
+            >
+              <FcConferenceCall size={100} />
+              <Text tag={'span'}>신입생 대상 세미나</Text>
+            </Container>
+            <Container
+              display={'flex'}
+              css={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                margin: '0 1rem',
+                '@mobile': {
+                  width: '100%',
+                  margin: '1rem 0',
+                },
+              }}
+            >
+              <FcSerialTasks size={100} />
+              <Text tag={'span'}>최신 웹 개발 커리큘럼</Text>
+            </Container>
+            <Container
+              display={'flex'}
+              css={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                margin: '0 1rem',
+                '@mobile': {
+                  width: '100%',
+                  margin: '1rem 0',
+                },
+              }}
+            >
+              <FcCollaboration size={100} />
+              <Text tag={'span'}>협업 프로젝트</Text>
+            </Container>
+            <Container
+              display={'flex'}
+              css={{
+                alignItems: 'center',
+                flexDirection: 'column',
+                margin: '0 1rem',
+                '@mobile': {
+                  width: '100%',
+                  margin: '1rem 0',
+                },
+              }}
+            >
+              <FcSearch size={100} />
+              <Text tag={'span'}>최신 기술 연구</Text>
+            </Container>
+          </Container>
         </Container>
         <Container
           display={'flex'}
@@ -117,8 +202,12 @@ export default function Home() {
             css={{
               fontSize: '2rem',
               maxWidth: '1200px',
-              padding: '0 1rem',
+              padding: '3rem 1rem',
               fontWeight: '300',
+              wordBreak: 'keep-all',
+              '@mobile': {
+                textAlign: 'left',
+              },
             }}
           >
             함께 웹 개발의 미래를 만들어가는 과정에 참여하세요. 웹사이트를 탐험하여 우리의 성취를
